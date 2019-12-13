09511f41cb98b709dec17e8300a822e46bc33543
Axel Gonon <gohon95@gmail.com>
2019-12-13 14:22:13 +0100

    First commit

---
+++ Jeux plus ou moins/Projet.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>) {
+
+    println("Joueur 1 : Entrez un nombre\n")
+    val secretNumber = readLine()!!.toInt()
+
+    var Joueur1: Int
+    var attempts=0
+
+    while (true) {
+        print("Joueur 2 : Devinez un nombre (1-100): ")
+        Joueur1 = readLine()!!.toInt()
+
+        when (Joueur1.compareTo(secretNumber)) {
+            -1 -> { println("Plus grand"); attempts++ }
+            0 -> { attempts++; println("Bravo ! Vous avez réussi en $attempts essais!"); return }
+            1 -> { println("Plus petit"); attempts++ }
+        }
+    }
+}
